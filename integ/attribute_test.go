94761b263947672b4d9fd5738f93bccced313818
Quanzheng Long <qlong@indeed.com>
2022-09-20 14:28:03 -0700

    add attribute test

---
+++ integ/attribute_test.go
@@ -0,0 +1,92 @@
+package integ
+
+import (
+	"context"
+	"fmt"
+	"github.com/cadence-oss/iwf-server/gen/iwfidl"
+	"github.com/cadence-oss/iwf-server/integ/attribute"
+	"github.com/cadence-oss/iwf-server/service/api"
+	"github.com/cadence-oss/iwf-server/service/interpreter/temporal"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"net/http"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestAttributeWorkflow(t *testing.T) {
+	// start test workflow server
+	wfHandler, basicWorkflow := attribute.NewAttributeWorkflow()
+	testWorkflowServerPort := "9714"
+	wfServer := &http.Server{
+		Addr:    ":" + testWorkflowServerPort,
+		Handler: basicWorkflow,
+	}
+	defer wfServer.Close()
+	go func() {
+		if err := wfServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// start iwf api server
+	temporalClient := createTemporalClient()
+	iwfService := api.NewService(temporalClient)
+	testIwfServerPort := "9715"
+	iwfServer := &http.Server{
+		Addr:    ":" + testIwfServerPort,
+		Handler: iwfService,
+	}
+	defer iwfServer.Close()
+	go func() {
+		if err := iwfServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// start iwf interpreter worker
+	interpreter := temporal.NewInterpreterWorker(temporalClient)
+	interpreter.Start()
+	defer interpreter.Close()
+
+	// start a workflow
+	apiClient := iwfidl.NewAPIClient(&iwfidl.Configuration{
+		Servers: []iwfidl.ServerConfiguration{
+			{
+				URL: "http://localhost:" + testIwfServerPort,
+			},
+		},
+	})
+	wfId := attribute.WorkflowType + strconv.Itoa(int(time.Now().Unix()))
+	req := apiClient.DefaultApi.ApiV1WorkflowStartPost(context.Background())
+	resp, httpResp, err := req.WorkflowStartRequest(iwfidl.WorkflowStartRequest{
+		WorkflowId:             iwfidl.PtrString(wfId),
+		IwfWorkflowType:        iwfidl.PtrString(attribute.WorkflowType),
+		WorkflowTimeoutSeconds: iwfidl.PtrInt32(10),
+		IwfWorkerUrl:           iwfidl.PtrString("http://localhost:" + testWorkflowServerPort),
+		StartStateId:           iwfidl.PtrString(attribute.State1),
+	}).Execute()
+	if err != nil {
+		log.Fatalf("Fail to invoke start api %v", err)
+	}
+	if httpResp.StatusCode != http.StatusOK {
+		log.Fatalf("Status not success" + httpResp.Status)
+	}
+	fmt.Println(*resp)
+	defer temporalClient.TerminateWorkflow(context.Background(), wfId, "", "terminate incase not completed")
+
+	// wait for the workflow
+	run := temporalClient.GetWorkflow(context.Background(), wfId, "")
+	_ = run.Get(context.Background(), nil)
+
+	history, data := wfHandler.GetTestResult()
+	assertions := assert.New(t)
+	assertions.Equalf(map[string]int64{
+		"S1_start":  1,
+		"S1_decide": 1,
+		"S2_start":  1,
+		"S2_decide": 1,
+	}, history, "attribute test fail, %v", history)
+	fmt.Println(data)
+}
