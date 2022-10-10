54ffbb3e93db5ef3c3a343078ae735e1f95f0865
Quanzheng Long <qlong@indeed.com>
2022-10-10 14:51:53 -0700

    done signal

--- integ/signal_test.go
+++ integ/signal_test.go
@@ -2,12 +2,9 @@ package integ
 
 import (
 	"context"
-	"fmt"
 	"github.com/cadence-oss/iwf-server/gen/iwfidl"
 	"github.com/cadence-oss/iwf-server/integ/signal"
-	"github.com/cadence-oss/iwf-server/service/api"
-	temporalapi "github.com/cadence-oss/iwf-server/service/api/temporal"
-	"github.com/cadence-oss/iwf-server/service/interpreter/temporal"
+	"github.com/cadence-oss/iwf-server/service"
 	"github.com/stretchr/testify/assert"
 	"log"
 	"net/http"
@@ -18,38 +15,12 @@ import (
 
 func TestSignalWorkflow(t *testing.T) {
 	// start test workflow server
-	wfHandler, basicWorkflow := signal.NewSignalWorkflow()
-	testWorkflowServerPort := "9714"
-	wfServer := &http.Server{
-		Addr:    ":" + testWorkflowServerPort,
-		Handler: basicWorkflow,
-	}
-	defer wfServer.Close()
-	go func() {
-		if err := wfServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
-		}
-	}()
+	wfHandler := signal.NewHandler()
+	closeFunc1 := startWorkflowWorker(wfHandler)
+	defer closeFunc1()
 
-	// start iwf api server
-	temporalClient := createTemporalClient()
-	iwfService := api.NewService(temporalapi.NewTemporalClient(temporalClient))
-	testIwfServerPort := "9715"
-	iwfServer := &http.Server{
-		Addr:    ":" + testIwfServerPort,
-		Handler: iwfService,
-	}
-	defer iwfServer.Close()
-	go func() {
-		if err := iwfServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
-		}
-	}()
-
-	// start iwf interpreter worker
-	interpreter := temporal.NewInterpreterWorker(temporalClient)
-	interpreter.Start()
-	defer interpreter.Close()
+	closeFunc2 := startIwfService(service.BackendTypeTemporal)
+	defer closeFunc2()
 
 	// start a workflow
 	apiClient := iwfidl.NewAPIClient(&iwfidl.Configuration{
@@ -61,7 +32,7 @@ func TestSignalWorkflow(t *testing.T) {
 	})
 	wfId := signal.WorkflowType + strconv.Itoa(int(time.Now().Unix()))
 	req := apiClient.DefaultApi.ApiV1WorkflowStartPost(context.Background())
-	resp, httpResp, err := req.WorkflowStartRequest(iwfidl.WorkflowStartRequest{
+	_, httpResp, err := req.WorkflowStartRequest(iwfidl.WorkflowStartRequest{
 		WorkflowId:             wfId,
 		IwfWorkflowType:        signal.WorkflowType,
 		WorkflowTimeoutSeconds: 10,
@@ -74,8 +45,6 @@ func TestSignalWorkflow(t *testing.T) {
 	if httpResp.StatusCode != http.StatusOK {
 		log.Fatalf("Status not success" + httpResp.Status)
 	}
-	fmt.Println(*resp)
-	defer temporalClient.TerminateWorkflow(context.Background(), wfId, "", "terminate incase not completed")
 
 	// signal the workflow
 	signalVal := iwfidl.EncodedObject{
@@ -95,8 +64,17 @@ func TestSignalWorkflow(t *testing.T) {
 	}
 
 	// wait for the workflow
-	run := temporalClient.GetWorkflow(context.Background(), wfId, "")
-	_ = run.Get(context.Background(), nil)
+	reqWait := apiClient.DefaultApi.ApiV1WorkflowGetWithLongWaitPost(context.Background())
+	_, httpResp, err = reqWait.WorkflowGetRequest(iwfidl.WorkflowGetRequest{
+		WorkflowId:   wfId,
+		NeedsResults: iwfidl.PtrBool(true),
+	}).Execute()
+	if err != nil {
+		log.Fatalf("Fail to invoke start api %v", err)
+	}
+	if httpResp.StatusCode != http.StatusOK {
+		log.Fatalf("Fail to get workflow" + httpResp.Status)
+	}
 
 	history, data := wfHandler.GetTestResult()
 	assertions := assert.New(t)
