7b0e8a1427ac2a66556c308578c0a08ac29d87c6
Quanzheng Long <qlong@indeed.com>
2022-12-20 15:27:49 -0800

    remove commented code

--- service/const.go
+++ service/const.go
@@ -1,5 +1,9 @@
 package service
 
+type (
+	BackendType string
+)
+
 const (
 	TaskQueue                         = "Interpreter_DEFAULT"
 	GracefulCompletingWorkflowStateId = "_SYS_GRACEFUL_COMPLETING_WORKFLOW"
@@ -9,57 +13,16 @@ const (
 	StateStartApi  = "/api/v1/workflowState/start"
 	StateDecideApi = "/api/v1/workflowState/decide"
 
-	//DeciderTypeAllCommandCompleted = "ALL_COMMAND_COMPLETED"
-	//DeciderTypeAnyCommandCompleted = "ANY_COMMAND_COMPLETED"
-
-	//TimerStatusFired     = "FIRED"
-	//TimerStatusScheduled = "SCHEDULED"
-	//
-	//SignalStatusWaiting  = "WAITING"
-	//SignalStatusReceived = "RECEIVED"
-	//
-	//InternStateChannelCommandStatusWaiting = "WAITING"
-	//InternStateChannelCommandReceived      = "RECEIVED"
-
-	//SearchAttributeValueTypeKeyword = "KEYWORD"
-	//SearchAttributeValueTypeInt     = "INT"
-
 	GetDataObjectsWorkflowQueryType = "GetDataObjects"
 
 	WorkflowErrorTypeUserWorkflowDecision = "UserWorkflowDecision"
 	WorkflowErrorTypeUserWorkflowError    = "UserWorkflowError"
 	WorkflowErrorTypeUserInternalError    = "InternalError"
 
-	//WorkflowStatusRunning       = "RUNNING"
-	//WorkflowStatusCompleted     = "COMPLETED"
-	//WorkflowStatusFailed        = "FAILED"
-	//WorkflowStatusTimeout       = "TIMEOUT"
-	//WorkflowStatusTerminated    = "TERMINATED"
-	//WorkflowStatusCanceled      = "CANCELED"
-	//WorkflowStatusContinueAsNew = "CONTINUED_AS_NEW"
-
 	SearchAttributeGlobalVersion     = "IwfGlobalWorkflowVersion"
 	SearchAttributeExecutingStateIds = "IwfExecutingStateIds"
 	SearchAttributeIwfWorkflowType   = "IwfWorkflowType"
 
-	//WorkflowIDReusePolicyAllowDuplicateFailedOnly = "ALLOW_DUPLICATE_FAILED_ONLY"
-	//WorkflowIDReusePolicyAllowDuplicate           = "ALLOW_DUPLICATE"
-	//WorkflowIDReusePolicyRejectDuplicate          = "REJECT_DUPLICATE"
-	//WorkflowIDReusePolicyTerminateIfRunning       = "TERMINATE_IF_RUNNING"
-	//
-	//LoadingTypeLoadPartialWithoutLocking = "LOAD_PARTIAL_WITHOUT_LOCKING"
-	//LoadingTypeLoadAllWithoutLocking     = "LOAD_ALL_WITHOUT_LOCKING"
+	BackendTypeCadence  BackendType = "cadence"
+	BackendTypeTemporal BackendType = "temporal"
 )
-
-type BackendType string
-
-const BackendTypeCadence BackendType = "cadence"
-const BackendTypeTemporal BackendType = "temporal"
-
-//type ResetType string
-//
-//const ResetTypeHistoryEventId ResetType = "HISTORY_EVENT_ID"
-//const ResetTypeBeginning ResetType = "BEGINNING"
-//const ResetTypeHistoryEventTime ResetType = "HISTORY_EVENT_TIME"
-//const ResetTypeStateId ResetType = "STATE_ID"
-//const ResetTypeStateExecutionId ResetType = "STATE_EXECUTION_ID"
