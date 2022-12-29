ca1ff62776ec133e9cdd693b1eee4f172d9a263d
Quanzheng Long <longquanzheng@users.noreply.github.com>
2022-12-28 21:39:53 -0800

    Improve docker compose startup script (#122)

--- docker-compose/init.sh
+++ docker-compose/init.sh
@@ -3,12 +3,15 @@
 # use new version of tctl so that we can skip the prompt
 tctl config set version next
 
-# TODO break the loop after commands are successful
-for run in {1..20}; do
+for run in {1..60}; do
   sleep 1
-  tctl namespace register default || true
-  tctl search-attribute  create -name IwfWorkflowType -type Keyword -y || true
-  tctl search-attribute  create -name IwfGlobalWorkflowVersion -type Int -y || true
-  tctl search-attribute  create -name IwfExecutingStateIds -type Keyword -y || true
+  echo "now trying to register iWF system search attributes..."
+  if tctl search-attribute  create -name IwfWorkflowType -type Keyword -y; then
+    break
+  fi
 done
 
+tctl search-attribute  create -name IwfGlobalWorkflowVersion -type Int -y
+tctl search-attribute  create -name IwfExecutingStateIds -type Keyword -y
+
+tail -f /dev/null
