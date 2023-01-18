c8524f9f3b0b9a057d14689aad52cb1ded402b42
Quanzheng Long <longquanzheng@users.noreply.github.com>
2023-01-18 10:25:03 -0800

    Provide autofix for worker port (#143)

--- service/interpreter/activityImpl.go
+++ service/interpreter/activityImpl.go
@@ -62,11 +62,17 @@ func StateDecide(ctx context.Context, backendType service.BackendType, input ser
 }
 
 func getIwfWorkerBaseUrlWithFix(url string) string {
-	autofix := os.Getenv("AUTO_FIX_WORKER_URL")
-	if autofix != "" {
-		url = strings.Replace(url, "localhost", autofix, 1)
-		url = strings.Replace(url, "127.0.0.1", autofix, 1)
+	autofixUrl := os.Getenv("AUTO_FIX_WORKER_URL")
+	if autofixUrl != "" {
+		url = strings.Replace(url, "localhost", autofixUrl, 1)
+		url = strings.Replace(url, "127.0.0.1", autofixUrl, 1)
 	}
+	autofixPortEnv := os.Getenv("AUTO_FIX_WORKER_PORT_FROM_ENV")
+	if autofixPortEnv != "" {
+		envVal := os.Getenv(autofixPortEnv)
+		url = strings.Replace(url, "$"+autofixPortEnv+"$", envVal, 1)
+	}
+
 	return url
 }
 
