03316d2621f72e40ac0ff2bd3ca15c0fa9d9d7ff
Quanzheng Long <longquanzheng@users.noreply.github.com>
2023-01-19 15:00:37 -0800

    Add Attempt and FirstAttemptTimestamp to request context (#145)

--- gen/iwfidl/docs/Context.md
+++ gen/iwfidl/docs/Context.md
@@ -8,6 +8,8 @@ Name | Type | Description | Notes
 **WorkflowRunId** | **string** |  | 
 **WorkflowStartedTimestamp** | **int64** |  | 
 **StateExecutionId** | **string** |  | 
+**FirstAttemptTimestamp** | Pointer to **int64** |  | [optional] 
+**Attempt** | Pointer to **int32** |  | [optional] 
 
 ## Methods
 
@@ -107,6 +109,56 @@ and a boolean to check if the value has been set.
 
 SetStateExecutionId sets StateExecutionId field to given value.
 
+
+### GetFirstAttemptTimestamp
+
+`func (o *Context) GetFirstAttemptTimestamp() int64`
+
+GetFirstAttemptTimestamp returns the FirstAttemptTimestamp field if non-nil, zero value otherwise.
+
+### GetFirstAttemptTimestampOk
+
+`func (o *Context) GetFirstAttemptTimestampOk() (*int64, bool)`
+
+GetFirstAttemptTimestampOk returns a tuple with the FirstAttemptTimestamp field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetFirstAttemptTimestamp
+
+`func (o *Context) SetFirstAttemptTimestamp(v int64)`
+
+SetFirstAttemptTimestamp sets FirstAttemptTimestamp field to given value.
+
+### HasFirstAttemptTimestamp
+
+`func (o *Context) HasFirstAttemptTimestamp() bool`
+
+HasFirstAttemptTimestamp returns a boolean if a field has been set.
+
+### GetAttempt
+
+`func (o *Context) GetAttempt() int32`
+
+GetAttempt returns the Attempt field if non-nil, zero value otherwise.
+
+### GetAttemptOk
+
+`func (o *Context) GetAttemptOk() (*int32, bool)`
+
+GetAttemptOk returns a tuple with the Attempt field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetAttempt
+
+`func (o *Context) SetAttempt(v int32)`
+
+SetAttempt sets Attempt field to given value.
+
+### HasAttempt
+
+`func (o *Context) HasAttempt() bool`
+
+HasAttempt returns a boolean if a field has been set.
 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
--- gen/iwfidl/model_context.go
+++ gen/iwfidl/model_context.go
@@ -20,6 +20,8 @@ type Context struct {
 	WorkflowRunId string `json:"workflowRunId"`
 	WorkflowStartedTimestamp int64 `json:"workflowStartedTimestamp"`
 	StateExecutionId string `json:"stateExecutionId"`
+	FirstAttemptTimestamp *int64 `json:"firstAttemptTimestamp,omitempty"`
+	Attempt *int32 `json:"attempt,omitempty"`
 }
 
 // NewContext instantiates a new Context object
@@ -139,6 +141,70 @@ func (o *Context) SetStateExecutionId(v string) {
 	o.StateExecutionId = v
 }
 
+// GetFirstAttemptTimestamp returns the FirstAttemptTimestamp field value if set, zero value otherwise.
+func (o *Context) GetFirstAttemptTimestamp() int64 {
+	if o == nil || isNil(o.FirstAttemptTimestamp) {
+		var ret int64
+		return ret
+	}
+	return *o.FirstAttemptTimestamp
+}
+
+// GetFirstAttemptTimestampOk returns a tuple with the FirstAttemptTimestamp field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *Context) GetFirstAttemptTimestampOk() (*int64, bool) {
+	if o == nil || isNil(o.FirstAttemptTimestamp) {
+    return nil, false
+	}
+	return o.FirstAttemptTimestamp, true
+}
+
+// HasFirstAttemptTimestamp returns a boolean if a field has been set.
+func (o *Context) HasFirstAttemptTimestamp() bool {
+	if o != nil && !isNil(o.FirstAttemptTimestamp) {
+		return true
+	}
+
+	return false
+}
+
+// SetFirstAttemptTimestamp gets a reference to the given int64 and assigns it to the FirstAttemptTimestamp field.
+func (o *Context) SetFirstAttemptTimestamp(v int64) {
+	o.FirstAttemptTimestamp = &v
+}
+
+// GetAttempt returns the Attempt field value if set, zero value otherwise.
+func (o *Context) GetAttempt() int32 {
+	if o == nil || isNil(o.Attempt) {
+		var ret int32
+		return ret
+	}
+	return *o.Attempt
+}
+
+// GetAttemptOk returns a tuple with the Attempt field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *Context) GetAttemptOk() (*int32, bool) {
+	if o == nil || isNil(o.Attempt) {
+    return nil, false
+	}
+	return o.Attempt, true
+}
+
+// HasAttempt returns a boolean if a field has been set.
+func (o *Context) HasAttempt() bool {
+	if o != nil && !isNil(o.Attempt) {
+		return true
+	}
+
+	return false
+}
+
+// SetAttempt gets a reference to the given int32 and assigns it to the Attempt field.
+func (o *Context) SetAttempt(v int32) {
+	o.Attempt = &v
+}
+
 func (o Context) MarshalJSON() ([]byte, error) {
 	toSerialize := map[string]interface{}{}
 	if true {
@@ -152,6 +218,12 @@ func (o Context) MarshalJSON() ([]byte, error) {
 	}
 	if true {
 		toSerialize["stateExecutionId"] = o.StateExecutionId
+	}
+	if !isNil(o.FirstAttemptTimestamp) {
+		toSerialize["firstAttemptTimestamp"] = o.FirstAttemptTimestamp
+	}
+	if !isNil(o.Attempt) {
+		toSerialize["attempt"] = o.Attempt
 	}
 	return json.Marshal(toSerialize)
 }
