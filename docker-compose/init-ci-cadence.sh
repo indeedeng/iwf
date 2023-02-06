bdf4815e2cdad1b50fa2b2aff88b442b0d3f2a79
Quanzheng Long <qlong@indeed.com>
2023-02-06 11:27:33 -0800

    Fix ci integ test with Cadence (#166)

--- docker-compose/init-ci-cadence.sh
+++ docker-compose/init-ci-cadence.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for run in {1..60}; do
+for run in {1..120}; do
   sleep 1
   echo "now trying to register iWF system search attributes..."
   if yes | cadence adm cl asa --search_attr_key IwfGlobalWorkflowVersion --search_attr_type 2; then
@@ -10,6 +10,12 @@ done
 
 yes | cadence adm cl asa --search_attr_key IwfExecutingStateIds --search_attr_type 1
 yes | cadence adm cl asa --search_attr_key IwfWorkflowType --search_attr_type 1
+
+# see https://github.com/indeedeng/iwf/blob/main/CONTRIBUTING.md#option-3-run-with-your-own-cadence-service
+echo "now sleep for 60s so that all the search attributes can take effect"
+
+sleep 70
+
 cadence --do default domain register
 
 tail -f /dev/null
