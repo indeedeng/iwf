563889831811f0bdac099a49cd85fe20fa4732e0
Quanzheng Long <qlong@indeed.com>
2023-02-06 11:18:48 -0800

    add echo

--- docker-compose/init-ci-cadence.sh
+++ docker-compose/init-ci-cadence.sh
@@ -11,8 +11,9 @@ done
 yes | cadence adm cl asa --search_attr_key IwfExecutingStateIds --search_attr_type 1
 yes | cadence adm cl asa --search_attr_key IwfWorkflowType --search_attr_type 1
 
-# sleep for 60s so that all the search attributes can take effect
 # see https://github.com/indeedeng/iwf/blob/main/CONTRIBUTING.md#option-3-run-with-your-own-cadence-service
+echo "now sleep for 60s so that all the search attributes can take effect"
+
 sleep 60
 
 cadence --do default domain register
