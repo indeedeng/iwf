cafa0e3f12771f59f4b27807af4eeec2683595a4
duoertai <duoertai@gmail.com>
2023-03-27 03:35:54 -0700

    add new integ test cases

--- integ/wf_canceled_test.go
+++ integ/wf_canceled_test.go
@@ -19,6 +19,12 @@ func TestWorkflowCanceledTemporal(t *testing.T) {
 	for i := 0; i < *repeatIntegTest; i++ {
 		doTestWorkflowCanceled(t, service.BackendTypeTemporal)
 		time.Sleep(time.Millisecond * time.Duration(*repeatInterval))
+
+		doTestWorkflowTerminated(t, service.BackendTypeTemporal)
+		time.Sleep(time.Millisecond * time.Duration(*repeatInterval))
+
+		doTestWorkflowFail(t, service.BackendTypeTemporal)
+		time.Sleep(time.Millisecond * time.Duration(*repeatInterval))
 	}
 }
 
@@ -28,6 +34,12 @@ func TestWorkflowCanceledCadence(t *testing.T) {
 	}
 	for i := 0; i < *repeatIntegTest; i++ {
 		doTestWorkflowCanceled(t, service.BackendTypeCadence)
+		time.Sleep(time.Millisecond * time.Duration(*repeatInterval))
+
+		doTestWorkflowTerminated(t, service.BackendTypeTemporal)
+		time.Sleep(time.Millisecond * time.Duration(*repeatInterval))
+
+		doTestWorkflowFail(t, service.BackendTypeTemporal)
 		time.Sleep(time.Millisecond * time.Duration(*repeatInterval))
 	}
 }
@@ -81,5 +93,109 @@ func doTestWorkflowCanceled(t *testing.T, backendType service.BackendType) {
 		WorkflowStatus: iwfidl.CANCELED,
 		ErrorType:      nil,
 		ErrorMessage:   nil,
+	}, resp, "response not expected")
+}
+
+func doTestWorkflowTerminated(t *testing.T, backendType service.BackendType) {
+	// start test workflow server
+	wfHandler := signal.NewHandler()
+	closeFunc1 := startWorkflowWorker(wfHandler)
+	defer closeFunc1()
+
+	closeFunc2 := startIwfService(backendType)
+	defer closeFunc2()
+
+	// start a workflow
+	apiClient := iwfidl.NewAPIClient(&iwfidl.Configuration{
+		Servers: []iwfidl.ServerConfiguration{
+			{
+				URL: "http://localhost:" + testIwfServerPort,
+			},
+		},
+	})
+	wfId := "wf-cancel-test" + strconv.Itoa(int(time.Now().UnixNano()))
+	req := apiClient.DefaultApi.ApiV1WorkflowStartPost(context.Background())
+	startResp, httpResp, err := req.WorkflowStartRequest(iwfidl.WorkflowStartRequest{
+		WorkflowId:             wfId,
+		IwfWorkflowType:        signal.WorkflowType,
+		WorkflowTimeoutSeconds: 10,
+		IwfWorkerUrl:           "http://localhost:" + testWorkflowServerPort,
+		StartStateId:           signal.State1,
+	}).Execute()
+	panicAtHttpError(err, httpResp)
+
+	reqCancel := apiClient.DefaultApi.ApiV1WorkflowStopPost(context.Background())
+	httpResp, err = reqCancel.WorkflowStopRequest(iwfidl.WorkflowStopRequest{
+		WorkflowId: wfId,
+		StopType:   iwfidl.TERMINATE.Ptr(),
+	}).Execute()
+	panicAtHttpError(err, httpResp)
+
+	// wait for the workflow
+	reqWait := apiClient.DefaultApi.ApiV1WorkflowGetWithWaitPost(context.Background())
+	resp, httpResp, err := reqWait.WorkflowGetRequest(iwfidl.WorkflowGetRequest{
+		WorkflowId: wfId,
+	}).Execute()
+	panicAtHttpError(err, httpResp)
+
+	assertions := assert.New(t)
+
+	assertions.Equalf(&iwfidl.WorkflowGetResponse{
+		WorkflowRunId:  startResp.GetWorkflowRunId(),
+		WorkflowStatus: iwfidl.TERMINATED,
+		ErrorType:      nil,
+		ErrorMessage:   nil,
+	}, resp, "response not expected")
+}
+
+func doTestWorkflowFail(t *testing.T, backendType service.BackendType) {
+	// start test workflow server
+	wfHandler := signal.NewHandler()
+	closeFunc1 := startWorkflowWorker(wfHandler)
+	defer closeFunc1()
+
+	closeFunc2 := startIwfService(backendType)
+	defer closeFunc2()
+
+	// start a workflow
+	apiClient := iwfidl.NewAPIClient(&iwfidl.Configuration{
+		Servers: []iwfidl.ServerConfiguration{
+			{
+				URL: "http://localhost:" + testIwfServerPort,
+			},
+		},
+	})
+	wfId := "wf-cancel-test" + strconv.Itoa(int(time.Now().UnixNano()))
+	req := apiClient.DefaultApi.ApiV1WorkflowStartPost(context.Background())
+	startResp, httpResp, err := req.WorkflowStartRequest(iwfidl.WorkflowStartRequest{
+		WorkflowId:             wfId,
+		IwfWorkflowType:        signal.WorkflowType,
+		WorkflowTimeoutSeconds: 10,
+		IwfWorkerUrl:           "http://localhost:" + testWorkflowServerPort,
+		StartStateId:           signal.State1,
+	}).Execute()
+	panicAtHttpError(err, httpResp)
+
+	reqCancel := apiClient.DefaultApi.ApiV1WorkflowStopPost(context.Background())
+	httpResp, err = reqCancel.WorkflowStopRequest(iwfidl.WorkflowStopRequest{
+		WorkflowId: wfId,
+		StopType:   iwfidl.FAIL.Ptr(),
+	}).Execute()
+	panicAtHttpError(err, httpResp)
+
+	// wait for the workflow
+	reqWait := apiClient.DefaultApi.ApiV1WorkflowGetWithWaitPost(context.Background())
+	resp, httpResp, err := reqWait.WorkflowGetRequest(iwfidl.WorkflowGetRequest{
+		WorkflowId: wfId,
+	}).Execute()
+	panicAtHttpError(err, httpResp)
+
+	assertions := assert.New(t)
+
+	assertions.Equalf(&iwfidl.WorkflowGetResponse{
+		WorkflowRunId:  startResp.GetWorkflowRunId(),
+		WorkflowStatus: iwfidl.FAILED,
+		ErrorType:      iwfidl.CLIENT_API_FAILING_WORKFLOW_ERROR_TYPE.Ptr(),
+		ErrorMessage:   iwfidl.PtrString("failed by client"),
 	}, resp, "response not expected")
 }
