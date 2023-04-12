d4c80894a2dbc6d679cfeb8764cc65e3b9f7f4b1
Quanzheng Long <qlong@indeed.com>
2023-04-12 14:54:29 -0700

    Workaround temporal search attribute registration issue (#245)

--- docker-compose/init-ci-temporal.sh
+++ docker-compose/init-ci-temporal.sh
@@ -3,7 +3,7 @@
 # use new version of tctl so that we can skip the prompt
 tctl config set version next
 
-for run in {1..60}; do
+for run in {1..120}; do
   sleep 1
   echo "now trying to register iWF system search attributes..."
   if tctl search-attribute  create -name IwfWorkflowType -type Keyword -y; then
@@ -11,14 +11,24 @@ for run in {1..60}; do
   fi
 done
 
+# see https://github.com/temporalio/temporal/issues/4160
+sleep 0.1
+tctl search-attribute  create -name IwfWorkflowType -type Keyword -y
+sleep 0.1
 tctl search-attribute  create -name IwfGlobalWorkflowVersion -type Int -y
+sleep 0.1
 tctl search-attribute  create -name IwfExecutingStateIds -type Keyword -y
-
+sleep 0.1
 tctl search-attribute  create -name CustomKeywordField -type Keyword -y
+sleep 0.1
 tctl search-attribute  create -name CustomIntField -type Int -y
+sleep 0.1
 tctl search-attribute  create -name CustomBoolField -type Bool -y
+sleep 0.1
 tctl search-attribute  create -name CustomDoubleField -type Double -y
+sleep 0.1
 tctl search-attribute  create -name CustomDatetimeField -type Datetime -y
+sleep 0.1
 tctl search-attribute  create -name CustomStringField -type text -y
 
 tail -f /dev/null
