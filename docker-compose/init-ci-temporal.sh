daa720ce97a9a0b4103a84bef94025b2b9c552f4
Quanzheng Long <qlong@indeed.com>
2023-04-18 10:02:27 -0700

    Add check for temporal sa

--- docker-compose/init-ci-temporal.sh
+++ docker-compose/init-ci-temporal.sh
@@ -3,32 +3,44 @@
 # use new version of tctl so that we can skip the prompt
 tctl config set version next
 
+checkExists () {
+  # see https://github.com/temporalio/temporal/issues/4160
+  if tctl search-attribute list | grep -q "$1"; then
+    return 0
+  else
+    return 1
+fi
+}
+
+echo "now trying to register iWF system search attributes..."
 for run in {1..120}; do
   sleep 1
-  echo "now trying to register iWF system search attributes..."
-  if tctl search-attribute  create -name IwfWorkflowType -type Keyword -y; then
+  tctl search-attribute  create -name IwfWorkflowType -type Keyword -y;
+  sleep 0.1
+  tctl search-attribute  create -name IwfWorkflowType -type Keyword -y
+  sleep 0.1
+  tctl search-attribute  create -name IwfGlobalWorkflowVersion -type Int -y
+  sleep 0.1
+  tctl search-attribute  create -name IwfExecutingStateIds -type Keyword -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomKeywordField -type Keyword -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomIntField -type Int -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomBoolField -type Bool -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomDoubleField -type Double -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomDatetimeField -type Datetime -y
+  sleep 0.1
+  tctl search-attribute  create -name CustomStringField -type text -y
+
+  if checkExists "IwfWorkflowType" ] && checkExists "IwfGlobalWorkflowVersion" && checkExists "IwfExecutingStateIds" && checkExists "CustomKeywordField" && checkExists "CustomIntField" && checkExists "CustomBoolField" && checkExists "CustomDoubleField" && checkExists "CustomDatetimeField" && checkExists "CustomStringField" ] ; then
+    echo "All search attributes are registered"
     break
   fi
+
 done
 
-# see https://github.com/temporalio/temporal/issues/4160
-sleep 0.1
-tctl search-attribute  create -name IwfWorkflowType -type Keyword -y
-sleep 0.1
-tctl search-attribute  create -name IwfGlobalWorkflowVersion -type Int -y
-sleep 0.1
-tctl search-attribute  create -name IwfExecutingStateIds -type Keyword -y
-sleep 0.1
-tctl search-attribute  create -name CustomKeywordField -type Keyword -y
-sleep 0.1
-tctl search-attribute  create -name CustomIntField -type Int -y
-sleep 0.1
-tctl search-attribute  create -name CustomBoolField -type Bool -y
-sleep 0.1
-tctl search-attribute  create -name CustomDoubleField -type Double -y
-sleep 0.1
-tctl search-attribute  create -name CustomDatetimeField -type Datetime -y
-sleep 0.1
-tctl search-attribute  create -name CustomStringField -type text -y
 
 tail -f /dev/null
--- docker-compose/init-compose.sh
+++ docker-compose/init-compose.sh
@@ -3,18 +3,31 @@
 # use new version of tctl so that we can skip the prompt
 tctl config set version next
 
+checkExists () {
+  # see https://github.com/temporalio/temporal/issues/4160
+  if tctl search-attribute list | grep -q "$1"; then
+    return 0
+  else
+    return 1
+fi
+}
+
+echo "now trying to register iWF system search attributes..."
+
 for run in {1..60}; do
   sleep 1
-  echo "now trying to register iWF system search attributes..."
-  if tctl search-attribute  create -name IwfWorkflowType -type Keyword -y; then
-    break
-  fi
+  tctl search-attribute  create -name IwfWorkflowType -type Keyword -y
+  sleep 0.1
+  tctl search-attribute  create -name IwfGlobalWorkflowVersion -type Int -y
+  sleep 0.1
+  tctl search-attribute  create -name IwfExecutingStateIds -type Keyword -y
+  sleep 0.1
+  if checkExists "IwfWorkflowType" ] && checkExists "IwfGlobalWorkflowVersion" && checkExists "IwfExecutingStateIds" ] ; then
+      echo "All search attributes are registered"
+      break
+    fi
 done
-sleep 0.1
-tctl search-attribute  create -name IwfGlobalWorkflowVersion -type Int -y
-sleep 0.1
-tctl search-attribute  create -name IwfExecutingStateIds -type Keyword -y
-sleep 0.1
+
 tctl namespace register default
 
 tail -f /dev/null
