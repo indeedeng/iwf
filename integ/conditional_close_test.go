d6118d0d3926f0c771dffe40c83a803db84fa23c
Quanzheng Long <qlong@indeed.com>
2023-07-04 15:47:03 -0700

    Add tests

---
+++ integ/conditional_close_test.go
@@ -0,0 +1,121 @@
+package integ
+
+import (
+	"context"
+	"github.com/indeedeng/iwf/gen/iwfidl"
+	conditionalClose "github.com/indeedeng/iwf/integ/workflow/conditional_close"
+	"github.com/indeedeng/iwf/service"
+	"github.com/indeedeng/iwf/service/common/ptr"
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestConditionalForceCompleteOnInternalChannelEmptyWorkflowTemporal(t *testing.T) {
+	if !*temporalIntegTest {
+		t.Skip()
+	}
+	for i := 0; i < *repeatIntegTest; i++ {
+		doTestConditionalForceCompleteOnInternalChannelEmptyWorkflow(t, service.BackendTypeTemporal, nil)
+		smallWaitForFastTest()
+	}
+}
+
+func TestConditionalForceCompleteOnInternalChannelEmptyWorkflowCadence(t *testing.T) {
+	if !*cadenceIntegTest {
+		t.Skip()
+	}
+	for i := 0; i < *repeatIntegTest; i++ {
+		doTestConditionalForceCompleteOnInternalChannelEmptyWorkflow(t, service.BackendTypeCadence, nil)
+		smallWaitForFastTest()
+	}
+}
+
+func TestConditionalForceCompleteOnInternalChannelEmptyWorkflowTemporalContinueAsNew(t *testing.T) {
+	if !*temporalIntegTest {
+		t.Skip()
+	}
+	for i := 0; i < *repeatIntegTest; i++ {
+		doTestConditionalForceCompleteOnInternalChannelEmptyWorkflow(t, service.BackendTypeTemporal, minimumContinueAsNewConfig())
+		smallWaitForFastTest()
+	}
+}
+
+func TestConditionalForceCompleteOnInternalChannelEmptyWorkflowCadenceContinueAsNew(t *testing.T) {
+	if !*cadenceIntegTest {
+		t.Skip()
+	}
+	for i := 0; i < *repeatIntegTest; i++ {
+		doTestConditionalForceCompleteOnInternalChannelEmptyWorkflow(t, service.BackendTypeCadence, minimumContinueAsNewConfig())
+		smallWaitForFastTest()
+	}
+}
+
+func doTestConditionalForceCompleteOnInternalChannelEmptyWorkflow(t *testing.T, backendType service.BackendType, config *iwfidl.WorkflowConfig) {
+	assertions := assert.New(t)
+	// start test workflow server
+	wfHandler := conditionalClose.NewHandler()
+	closeFunc1 := startWorkflowWorkerWithRpc(wfHandler)
+	defer closeFunc1()
+
+	_, closeFunc2 := startIwfServiceWithClient(backendType)
+	defer closeFunc2()
+
+	// create client
+	apiClient := iwfidl.NewAPIClient(&iwfidl.Configuration{
+		Servers: []iwfidl.ServerConfiguration{
+			{
+				URL: "http://localhost:" + testIwfServerPort,
+			},
+		},
+	})
+
+	// start a workflow
+	wfId := conditionalClose.WorkflowType + strconv.Itoa(int(time.Now().UnixNano()))
+	req := apiClient.DefaultApi.ApiV1WorkflowStartPost(context.Background())
+	_, httpResp, err := req.WorkflowStartRequest(iwfidl.WorkflowStartRequest{
+		WorkflowId:             wfId,
+		IwfWorkflowType:        conditionalClose.WorkflowType,
+		WorkflowTimeoutSeconds: 20,
+		IwfWorkerUrl:           "http://localhost:" + testWorkflowServerPort,
+		StartStateId:           ptr.Any(conditionalClose.State1),
+		WorkflowStartOptions: &iwfidl.WorkflowStartOptions{
+			WorkflowConfigOverride: config,
+		},
+	}).Execute()
+	panicAtHttpError(err, httpResp)
+
+	// invoke RPCs to send 3 messages to the internal channel
+	reqRpc := apiClient.DefaultApi.ApiV1WorkflowRpcPost(context.Background())
+	for i := 0; i < 3; i++ {
+		time.Sleep(time.Second * 2)
+		_, httpResp, err = reqRpc.WorkflowRpcRequest(iwfidl.WorkflowRpcRequest{
+			WorkflowId: wfId,
+			RpcName:    conditionalClose.RpcPublishInternalChannel,
+		}).Execute()
+		panicAtHttpError(err, httpResp)
+	}
+
+	// wait for the workflow
+	req2 := apiClient.DefaultApi.ApiV1WorkflowGetWithWaitPost(context.Background())
+	resp2, httpResp, err := req2.WorkflowGetRequest(iwfidl.WorkflowGetRequest{
+		WorkflowId: wfId,
+	}).Execute()
+	panicAtHttpError(err, httpResp)
+
+	history, _ := wfHandler.GetTestResult()
+	assertions.Equalf(map[string]int64{
+		"S1_start":               3,
+		"S1_decide":              3,
+		"ApiV1WorkflowWorkerRpc": 3,
+	}, history, "rpc test fail, %v", history)
+
+	assertions.Equal(iwfidl.COMPLETED, resp2.GetWorkflowStatus())
+	assertions.Equal(1, len(resp2.GetResults()))
+	assertions.Equal(iwfidl.StateCompletionOutput{
+		CompletedStateId:          "S1",
+		CompletedStateExecutionId: "S1-3",
+		CompletedStateOutput:      &conditionalClose.TestInput,
+	}, resp2.GetResults()[0])
+}
---
+++ integ/workflow/conditional_close/routers.go
@@ -0,0 +1,125 @@
+package conditional_close
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/indeedeng/iwf/gen/iwfidl"
+	"github.com/indeedeng/iwf/integ/workflow/common"
+	"github.com/indeedeng/iwf/service/common/ptr"
+	"log"
+	"net/http"
+	"time"
+)
+
+const (
+	WorkflowType              = "conditional_close"
+	RpcPublishInternalChannel = "publish_internal_channel"
+
+	InternalChannelName = "test-channel-name"
+
+	State1 = "S1"
+)
+
+var TestInput = iwfidl.EncodedObject{
+	Encoding: iwfidl.PtrString("json"),
+	Data:     iwfidl.PtrString("test-data"),
+}
+
+type handler struct {
+	invokeHistory map[string]int64
+	invokeData    map[string]interface{}
+}
+
+func NewHandler() common.WorkflowHandlerWithRpc {
+	return &handler{
+		invokeHistory: make(map[string]int64),
+		invokeData:    make(map[string]interface{}),
+	}
+}
+
+func (h *handler) ApiV1WorkflowWorkerRpc(c *gin.Context) {
+	var req iwfidl.WorkflowWorkerRpcRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	log.Println("received workflow worker rpc request, ", req)
+	h.invokeHistory["ApiV1WorkflowWorkerRpc"]++
+
+	c.JSON(http.StatusOK, iwfidl.WorkflowWorkerRpcResponse{
+		PublishToInterStateChannel: []iwfidl.InterStateChannelPublishing{
+			{
+				ChannelName: RpcPublishInternalChannel,
+			},
+		},
+	})
+}
+
+// ApiV1WorkflowStateStart - for a workflow
+func (h *handler) ApiV1WorkflowStateStart(c *gin.Context) {
+	var req iwfidl.WorkflowStateStartRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	log.Println("received state start request, ", req)
+
+	if req.GetWorkflowType() == WorkflowType {
+		h.invokeHistory[req.GetWorkflowStateId()+"_start"]++
+		if req.GetWorkflowStateId() == State1 {
+
+			c.JSON(http.StatusOK, iwfidl.WorkflowStateStartResponse{
+				CommandRequest: &iwfidl.CommandRequest{
+					InterStateChannelCommands: []iwfidl.InterStateChannelCommand{
+						{
+							ChannelName: InternalChannelName,
+						},
+					},
+					CommandWaitingType: ptr.Any(iwfidl.ANY_COMPLETED),
+				},
+			})
+			return
+		}
+	}
+
+	panic("error request")
+}
+
+func (h *handler) ApiV1WorkflowStateDecide(c *gin.Context) {
+	var req iwfidl.WorkflowStateDecideRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	log.Println("received state decide request, ", req)
+
+	if req.GetWorkflowType() == WorkflowType {
+		h.invokeHistory[req.GetWorkflowStateId()+"_decide"]++
+		if req.GetWorkflowStateId() == State1 {
+
+			// wait for a second so that the channel can have a new message
+			time.Sleep(time.Second * 1)
+
+			c.JSON(http.StatusOK, iwfidl.WorkflowStateDecideResponse{
+				StateDecision: &iwfidl.StateDecision{
+					NextStates: []iwfidl.StateMovement{
+						{
+							StateId: State1,
+						},
+					},
+					ConditionalClose: &iwfidl.WorkflowConditionalClose{
+						ConditionalCloseType: iwfidl.FORCE_COMPLETE_ON_INTERNAL_CHANNEL_EMPTY.Ptr(),
+						ChannelName:          iwfidl.PtrString(InternalChannelName),
+						CloseInput:           &TestInput,
+					},
+				},
+			})
+			return
+		}
+	}
+
+	panic("error request")
+}
+
+func (h *handler) GetTestResult() (map[string]int64, map[string]interface{}) {
+	return h.invokeHistory, h.invokeData
+}
