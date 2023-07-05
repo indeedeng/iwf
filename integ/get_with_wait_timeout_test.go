30d696d7bab7c5aeff8fa6ab50f9261a1c61e4e5
Quanzheng Long <qlong@indeed.com>
2023-07-05 11:42:26 -0700

    revert idl

---
+++ integ/get_with_wait_timeout_test.go
@@ -0,0 +1,85 @@
+package integ
+
+import (
+	"context"
+	"github.com/indeedeng/iwf/gen/iwfidl"
+	"github.com/indeedeng/iwf/integ/workflow/signal"
+	"github.com/indeedeng/iwf/service"
+	"github.com/indeedeng/iwf/service/common/ptr"
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestWorkflowTimeoutTemporal(t *testing.T) {
+	if !*temporalIntegTest {
+		t.Skip()
+	}
+	for i := 0; i < *repeatIntegTest; i++ {
+		doTestWorkflowTimeout(t, service.BackendTypeTemporal, nil)
+		smallWaitForFastTest()
+		doTestWorkflowTimeout(t, service.BackendTypeTemporal, minimumContinueAsNewConfig())
+		smallWaitForFastTest()
+	}
+}
+
+func TestWorkflowTimeoutadence(t *testing.T) {
+	if !*cadenceIntegTest {
+		t.Skip()
+	}
+	for i := 0; i < *repeatIntegTest; i++ {
+		doTestWorkflowTimeout(t, service.BackendTypeCadence, nil)
+		smallWaitForFastTest()
+		doTestWorkflowTimeout(t, service.BackendTypeCadence, minimumContinueAsNewConfig())
+		smallWaitForFastTest()
+	}
+}
+
+func doTestWorkflowTimeout(t *testing.T, backendType service.BackendType, config *iwfidl.WorkflowConfig) {
+	// start test workflow server
+	wfHandler := signal.NewHandler()
+	closeFunc1 := startWorkflowWorker(wfHandler)
+	defer closeFunc1()
+
+	closeFunc2 := startIwfService(backendType)
+	defer closeFunc2()
+
+	// start a workflow
+	apiClient := iwfidl.NewAPIClient(&iwfidl.Configuration{
+		Servers: []iwfidl.ServerConfiguration{
+			{
+				URL: "http://localhost:" + testIwfServerPort,
+			},
+		},
+	})
+	wfId := "wf-timeout-test" + strconv.Itoa(int(time.Now().UnixNano()))
+	req := apiClient.DefaultApi.ApiV1WorkflowStartPost(context.Background())
+	startResp, httpResp, err := req.WorkflowStartRequest(iwfidl.WorkflowStartRequest{
+		WorkflowId:             wfId,
+		IwfWorkflowType:        signal.WorkflowType,
+		WorkflowTimeoutSeconds: 1,
+		IwfWorkerUrl:           "http://localhost:" + testWorkflowServerPort,
+		StartStateId:           ptr.Any(signal.State1),
+		WorkflowStartOptions: &iwfidl.WorkflowStartOptions{
+			WorkflowConfigOverride: config,
+		},
+	}).Execute()
+	panicAtHttpError(err, httpResp)
+
+	// wait for the workflow
+	reqWait := apiClient.DefaultApi.ApiV1WorkflowGetWithWaitPost(context.Background())
+	resp, httpResp, err := reqWait.WorkflowGetRequest(iwfidl.WorkflowGetRequest{
+		WorkflowId: wfId,
+	}).Execute()
+	panicAtHttpError(err, httpResp)
+
+	assertions := assert.New(t)
+
+	assertions.Equalf(&iwfidl.WorkflowGetResponse{
+		WorkflowRunId:  startResp.GetWorkflowRunId(),
+		WorkflowStatus: iwfidl.TIMEOUT,
+		ErrorType:      nil,
+		ErrorMessage:   nil,
+	}, resp, "response not expected")
+}
