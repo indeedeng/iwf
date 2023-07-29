f705a73be67d1a0ad6e01975558cba481cfc54cf
Quanzheng Long <qlong@indeed.com>
2023-07-29 16:38:32 -0700

    Add iwf lite image

---
+++ lite/README.md
@@ -0,0 +1,15 @@
+
+An all-in-one image for iWF server.
+
+(Internally, it just runs Temporalite as backend)
+## How to use
+```shell
+docker run -p 8801:8801 -p 7933:7933 -p 8233:8233 -e AUTO_FIX_WORKER_URL=host.docker.internal --add-host host.docker.internal:host-gateway -it iworkflowio/iwf-server-lite:latest
+```
+
+## How to build
+Make sure you are at the root directory of this project (parent of current):
+```shell
+docker build . -t iworkflowio/iwf-server-lite:<yourTag> -f lite/Dockerfile
+```
+
---
+++ lite/start-lite-server.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+checkExists () {
+  # see https://github.com/temporalio/temporal/issues/4160
+  if temporal  operator search-attribute list | grep -q "$1"; then
+    return 0
+  else
+    return 1
+fi
+}
+
+export PATH="$PATH:/root/.temporalio/bin"
+temporal server start-dev --ip 0.0.0.0 --ui-ip 0.0.0.0 &
+# add SAs...
+echo "temporal server started..."
+echo "now trying to register iWF system search attributes..."
+
+for run in {1..60}; do
+  sleep 1
+  temporal  operator search-attribute  create -name IwfWorkflowType -type Keyword
+  sleep 0.1
+  temporal  operator search-attribute  create -name IwfGlobalWorkflowVersion -type Int 
+  sleep 0.1
+  temporal  operator search-attribute  create -name IwfExecutingStateIds -type Keyword 
+  sleep 0.1
+  if checkExists "IwfWorkflowType" ] && checkExists "IwfGlobalWorkflowVersion" && checkExists "IwfExecutingStateIds" ] ; then
+      echo "All search attributes are registered"
+      break
+    fi
+done
+
+/iwf/iwf-server start
