ea86a770ae62ae40a92e6c060e07c25e2fbf81a8
Quanzheng Long <qlong@indeed.com>
2023-08-02 15:13:50 -0700

    fix

--- docker-compose/init-compose.sh
+++ docker-compose/init-compose.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# use new version of tctl so that we can skip the prompt
-tctl config set version next
-
 checkExists () {
   # see https://github.com/temporalio/temporal/issues/4160
-  if tctl search-attribute list | grep -q "$1"; then
+  if temporal  operator search-attribute list | grep -q "$1"; then
     return 0
   else
     return 1
@@ -16,11 +13,11 @@ echo "now trying to register iWF system search attributes..."
 
 for run in {1..60}; do
   sleep 1
-  tctl search-attribute  create -name IwfWorkflowType -type Keyword -y
+  temporal  operator search-attribute  create -name IwfWorkflowType -type Keyword
   sleep 0.1
-  tctl search-attribute  create -name IwfGlobalWorkflowVersion -type Int -y
-  sleep 0.1
-  tctl search-attribute  create -name IwfExecutingStateIds -type Keyword -y
+  temporal  operator search-attribute  create -name IwfGlobalWorkflowVersion -type Int
+  sleep 0.1 
+  temporal  operator search-attribute  create -name IwfExecutingStateIds -type KeywordList 
   sleep 0.1
   if checkExists "IwfWorkflowType" ] && checkExists "IwfGlobalWorkflowVersion" && checkExists "IwfExecutingStateIds" ] ; then
       echo "All search attributes are registered"
