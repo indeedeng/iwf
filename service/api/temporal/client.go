6eb39ad5601ee099da6378fc538f59b7b898194c
Quanzheng Long <qlong@indeed.com>
2024-02-21 12:15:14 -0800

    improve wait workflow timeout

--- service/api/temporal/client.go
+++ service/api/temporal/client.go
@@ -154,7 +154,7 @@ func (t *temporalClient) StartWaitForStateCompletionWorkflow(
 ) (runId string, err error) {
 	workflowOptions := client.StartWorkflowOptions{
 		ID:                       options.ID,
-		WorkflowIDReusePolicy:    enums.WORKFLOW_ID_REUSE_POLICY_REJECT_DUPLICATE, //ALLOW_DUPLICATE_FAILED_ONLY, // the workflow could be timeout, so we allow duplicate
+		WorkflowIDReusePolicy:    enums.WORKFLOW_ID_REUSE_POLICY_ALLOW_DUPLICATE_FAILED_ONLY, // the workflow could be timeout, so we allow duplicate
 		TaskQueue:                options.TaskQueue,
 		WorkflowExecutionTimeout: options.WorkflowExecutionTimeout,
 	}
