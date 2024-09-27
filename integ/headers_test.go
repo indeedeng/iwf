470b97a95dca2aac55789f754f6290d1f4dd9d32
Samuel Caçador <samuelmoreira27@gmail.com>
2024-09-27 15:24:39 +0100

    Simplify workflow used for headers testing

--- integ/headers_test.go
+++ integ/headers_test.go
@@ -2,11 +2,8 @@ package integ
 
 import (
 	"context"
-	"github.com/indeedeng/iwf/integ/workflow/basic"
 	"github.com/indeedeng/iwf/integ/workflow/headers"
 	"github.com/indeedeng/iwf/service/common/ptr"
-	"github.com/indeedeng/iwf/service/interpreter/env"
-	"log"
 	"strconv"
 	"testing"
 	"time"
@@ -20,6 +17,7 @@ func TestHeadersWorkflowTemporal(t *testing.T) {
 	if !*temporalIntegTest {
 		t.Skip()
 	}
+
 	for i := 0; i < *repeatIntegTest; i++ {
 		doTestWorkflowWithHeaders(t, service.BackendTypeTemporal, nil)
 		smallWaitForFastTest()
@@ -61,16 +59,14 @@ func doTestWorkflowWithHeaders(t *testing.T, backendType service.BackendType, co
 	defer closeFunc2()
 
 	// start a workflow
-	sharedConfig := env.GetSharedConfig()
 	apiClient := iwfidl.NewAPIClient(&iwfidl.Configuration{
-		DefaultHeader: sharedConfig.Interpreter.InterpreterActivityConfig.DefaultHeader,
 		Servers: []iwfidl.ServerConfiguration{
 			{
 				URL: "http://localhost:" + testIwfServerPort,
 			},
 		},
 	})
-	wfId := basic.WorkflowType + strconv.Itoa(int(time.Now().UnixNano()))
+	wfId := headers.WorkflowType + strconv.Itoa(int(time.Now().UnixNano()))
 	wfInput := &iwfidl.EncodedObject{
 		Encoding: iwfidl.PtrString("json"),
 		Data:     iwfidl.PtrString("test data"),
@@ -78,90 +74,29 @@ func doTestWorkflowWithHeaders(t *testing.T, backendType service.BackendType, co
 	req := apiClient.DefaultApi.ApiV1WorkflowStartPost(context.Background())
 	startReq := iwfidl.WorkflowStartRequest{
 		WorkflowId:             wfId,
-		IwfWorkflowType:        basic.WorkflowType,
+		IwfWorkflowType:        headers.WorkflowType,
 		WorkflowTimeoutSeconds: 100,
 		IwfWorkerUrl:           "http://localhost:" + testWorkflowServerPort,
-		StartStateId:           ptr.Any(basic.State1),
+		StartStateId:           ptr.Any(headers.State1),
 		StateInput:             wfInput,
 		WorkflowStartOptions: &iwfidl.WorkflowStartOptions{
 			WorkflowConfigOverride: config,
-			WorkflowIDReusePolicy:  ptr.Any(iwfidl.REJECT_DUPLICATE),
-			// TODO: need more work to write integ test for cron
-			// manual testing for now by uncomment the following line
-			// CronSchedule:           iwfidl.PtrString("* * * * *"),
-			RetryPolicy: &iwfidl.WorkflowRetryPolicy{
-				InitialIntervalSeconds: iwfidl.PtrInt32(11),
-				BackoffCoefficient:     iwfidl.PtrFloat32(11),
-				MaximumAttempts:        iwfidl.PtrInt32(11),
-				MaximumIntervalSeconds: iwfidl.PtrInt32(11),
-			},
-		},
-		StateOptions: &iwfidl.WorkflowStateOptions{
-			StartApiTimeoutSeconds:  iwfidl.PtrInt32(12),
-			DecideApiTimeoutSeconds: iwfidl.PtrInt32(13),
-			StartApiRetryPolicy: &iwfidl.RetryPolicy{
-				InitialIntervalSeconds: iwfidl.PtrInt32(12),
-				BackoffCoefficient:     iwfidl.PtrFloat32(12),
-				MaximumAttempts:        iwfidl.PtrInt32(12),
-				MaximumIntervalSeconds: iwfidl.PtrInt32(12),
-			},
-			DecideApiRetryPolicy: &iwfidl.RetryPolicy{
-				InitialIntervalSeconds: iwfidl.PtrInt32(13),
-				BackoffCoefficient:     iwfidl.PtrFloat32(13),
-				MaximumAttempts:        iwfidl.PtrInt32(13),
-				MaximumIntervalSeconds: iwfidl.PtrInt32(13),
-			},
 		},
 	}
 	_, httpResp, err := req.WorkflowStartRequest(startReq).Execute()
 	panicAtHttpError(err, httpResp)
 
-	// start it again should return already started error
-	_, _, err = req.WorkflowStartRequest(startReq).Execute()
-	apiErr, ok := err.(*iwfidl.GenericOpenAPIError)
-	if !ok {
-		log.Fatalf("Should fail to invoke start api %v", err)
-	}
-	errResp, ok := apiErr.Model().(iwfidl.ErrorResponse)
-	if !ok {
-		log.Fatalf("should be error response")
-	}
-	assertions := assert.New(t)
-	assertions.Equal(errResp.GetSubStatus(), iwfidl.WORKFLOW_ALREADY_STARTED_SUB_STATUS)
-
 	req2 := apiClient.DefaultApi.ApiV1WorkflowGetWithWaitPost(context.Background())
-	resp2, httpResp, err := req2.WorkflowGetRequest(iwfidl.WorkflowGetRequest{
+	_, httpResp2, err2 := req2.WorkflowGetRequest(iwfidl.WorkflowGetRequest{
 		WorkflowId: wfId,
 	}).Execute()
-	panicAtHttpError(err, httpResp)
+	panicAtHttpError(err2, httpResp2)
 
-	// use a wrong workflowId to test the error case
-	_, _, err = req2.WorkflowGetRequest(iwfidl.WorkflowGetRequest{
-		WorkflowId: "a wrong workflowId",
-	}).Execute()
-	apiErr, ok = err.(*iwfidl.GenericOpenAPIError)
-	if !ok {
-		log.Fatalf("Should fail to invoke get api %v", err)
-	}
-	errResp, ok = apiErr.Model().(iwfidl.ErrorResponse)
-	if !ok {
-		log.Fatalf("should be error response")
-	}
-	assertions.Equal(errResp.GetSubStatus(), iwfidl.WORKFLOW_NOT_EXISTS_SUB_STATUS)
+	assertions := assert.New(t)
 
 	history, _ := wfHandler.GetTestResult()
 	assertions.Equalf(map[string]int64{
 		"S1_start":  1,
 		"S1_decide": 1,
-		"S2_start":  1,
-		"S2_decide": 1,
-	}, history, "basic test fail, %v", history)
-
-	assertions.Equal(iwfidl.COMPLETED, resp2.GetWorkflowStatus())
-	assertions.Equal(1, len(resp2.GetResults()))
-	assertions.Equal(iwfidl.StateCompletionOutput{
-		CompletedStateId:          "S2",
-		CompletedStateExecutionId: "S2-1",
-		CompletedStateOutput:      wfInput,
-	}, resp2.GetResults()[0])
+	}, history, "headers test fail, %v", history)
 }
--- integ/workflow/headers/routers.go
+++ integ/workflow/headers/routers.go
@@ -9,9 +9,8 @@ import (
 )
 
 const (
-	WorkflowType = "basic"
+	WorkflowType = "headers"
 	State1       = "S1"
-	State2       = "S2"
 )
 
 type handler struct {
@@ -28,7 +27,7 @@ func NewHandler() *handler {
 func (h *handler) ApiV1WorkflowStateStart(c *gin.Context) {
 	headerValue := c.GetHeader(TestHeaderKey)
 	if headerValue != TestHeaderValue {
-		panic("test header not found")
+		c.JSON(http.StatusBadRequest, gin.H{"error": "test header not found"})
 	}
 
 	var req iwfidl.WorkflowStateStartRequest
@@ -38,14 +37,9 @@ func (h *handler) ApiV1WorkflowStateStart(c *gin.Context) {
 	}
 	log.Println("received state start request, ", req)
 
-	context := req.GetContext()
-	if context.GetAttempt() <= 0 || context.GetFirstAttemptTimestamp() <= 0 {
-		panic("attempt and firstAttemptTimestamp should be greater than zero")
-	}
-
 	if req.GetWorkflowType() == WorkflowType {
 		// basic workflow go straight to decide methods without any commands
-		if req.GetWorkflowStateId() == State1 || req.GetWorkflowStateId() == State2 {
+		if req.GetWorkflowStateId() == State1 {
 			h.invokeHistory[req.GetWorkflowStateId()+"_start"]++
 			c.JSON(http.StatusOK, iwfidl.WorkflowStateStartResponse{
 				CommandRequest: &iwfidl.CommandRequest{
@@ -62,7 +56,7 @@ func (h *handler) ApiV1WorkflowStateStart(c *gin.Context) {
 func (h *handler) ApiV1WorkflowStateDecide(c *gin.Context) {
 	headerValue := c.GetHeader(TestHeaderKey)
 	if headerValue != TestHeaderValue {
-		panic("test header not found")
+		c.JSON(http.StatusBadRequest, gin.H{"error": "test header not found"})
 	}
 
 	var req iwfidl.WorkflowStateDecideRequest
@@ -71,44 +65,11 @@ func (h *handler) ApiV1WorkflowStateDecide(c *gin.Context) {
 		return
 	}
 	log.Println("received state decide request, ", req)
-	context := req.GetContext()
-	if context.GetAttempt() <= 0 || context.GetFirstAttemptTimestamp() <= 0 {
-		panic("attempt and firstAttemptTimestamp should be greater than zero")
-	}
 
 	if req.GetWorkflowType() == WorkflowType {
 		h.invokeHistory[req.GetWorkflowStateId()+"_decide"]++
 		if req.GetWorkflowStateId() == State1 {
 			// go to S2
-			c.JSON(http.StatusOK, iwfidl.WorkflowStateDecideResponse{
-				StateDecision: &iwfidl.StateDecision{
-					NextStates: []iwfidl.StateMovement{
-						{
-							StateId:    State2,
-							StateInput: req.StateInput,
-							StateOptions: &iwfidl.WorkflowStateOptions{
-								StartApiTimeoutSeconds:   iwfidl.PtrInt32(14),
-								ExecuteApiTimeoutSeconds: iwfidl.PtrInt32(15),
-								StartApiRetryPolicy: &iwfidl.RetryPolicy{
-									InitialIntervalSeconds: iwfidl.PtrInt32(14),
-									BackoffCoefficient:     iwfidl.PtrFloat32(14),
-									MaximumAttempts:        iwfidl.PtrInt32(14),
-									MaximumIntervalSeconds: iwfidl.PtrInt32(14),
-								},
-								ExecuteApiRetryPolicy: &iwfidl.RetryPolicy{
-									InitialIntervalSeconds: iwfidl.PtrInt32(15),
-									BackoffCoefficient:     iwfidl.PtrFloat32(15),
-									MaximumAttempts:        iwfidl.PtrInt32(15),
-									MaximumIntervalSeconds: iwfidl.PtrInt32(15),
-								},
-							},
-						},
-					},
-				},
-			})
-			return
-		} else if req.GetWorkflowStateId() == State2 {
-			// go to complete
 			c.JSON(http.StatusOK, iwfidl.WorkflowStateDecideResponse{
 				StateDecision: &iwfidl.StateDecision{
 					NextStates: []iwfidl.StateMovement{
