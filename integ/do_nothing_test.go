03994510204471249bff320e0a81a3d51ce1af8f
lwolczynski <lwolczynski7@gmail.com>
2024-10-10 13:59:28 -0500

    IWF-186: Add nolint to a test

--- integ/do_nothing_test.go
+++ integ/do_nothing_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 // remove the underscore to run
+// nolint
 func _TestNothingButJustRunningTheServiceTemporalWorkerForDebug(t *testing.T) {
 	startIwfServiceWithClient(service.BackendTypeTemporal)
 	time.Sleep(time.Hour)
