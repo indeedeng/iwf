cc8a67413ceec1b6a57180dedb33e17bc22a9c7c
Quanzheng Long <qlong@indeed.com>
2024-10-22 13:42:21 -0700

    Use firstRunId for workflow context (#462)

--- service/interpreter/cadence/workflowProvider.go
+++ service/interpreter/cadence/workflowProvider.go
@@ -85,7 +85,7 @@ func (w *workflowProvider) GetWorkflowInfo(ctx interpreter.UnifiedContext) inter
 		},
 		WorkflowStartTime:        time.UnixMilli(0), // TODO need support from Cadence client: https://github.com/uber-go/cadence-client/issues/1204
 		WorkflowExecutionTimeout: time.Duration(info.ExecutionStartToCloseTimeoutSeconds) * time.Second,
-		FirstRunID:               "", // Cadence does not provide FirstRunID
+		FirstRunID:               info.WorkflowExecution.RunID, // Cadence does not provide FirstRunID TODO https://github.com/uber-go/cadence-client/issues/1371 use firstRunID when available
 	}
 }
 
