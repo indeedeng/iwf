2a659ea57e9c0583ee036e826d884cacc696bb6b
Quanzheng Long <qlong@indeed.com>
2024-11-26 14:28:31 -0800

    Add replay test to reproduce bug about reset workflow with updates

--- replayTests/replay_test.go
+++ replayTests/replay_test.go
@@ -9,23 +9,7 @@ import (
 )
 
 var jsonHistoryFiles = []string{
-	"v1-persistence.json",
-	"v2-persistence.json",
-	"v2-basic.json",
-	"v2-basic-disable-system-searchattributes.json",
-	"v2-any-timer-signal.json",
-	"v3-any-timer-signal-continue-as-new.json",
-	"v3-basic.json",
-	"v3-skip-start.json",
-	"v3-bug-no-state-stuck.json",
-	"v4-continue-as-new-on-no-state.json",
-	"v4-continued-as-new-before-versioning-optimization.json",
-	"v4-local-activity-optimization.json",
-	"v5-basic.json",
-	"v6-search-attributes-optimization-enabled-for-all.json",
-	"v6-search-attributes-optimization-default.json",
-	"v7-no-global-version-search-attribute.json",
-	"v8-yield-on-conditional-complete.json",
+	"reset-update-reapply.json",
 }
 
 func TestTemporalReplay(t *testing.T) {
