ecd6442bb25e6b1ffaa5e3643667425975fb5337
Quanzheng Long <qlong@indeed.com>
2024-12-06 21:19:23 -0800

    done

--- docker-compose/init-ci-cadence.sh
+++ docker-compose/init-ci-cadence.sh
@@ -12,6 +12,12 @@ yes | cadence adm cl asa --search_attr_key IwfExecutingStateIds --search_attr_ty
 yes | cadence adm cl asa --search_attr_key IwfWorkflowType --search_attr_type 1
 
 
+echo "After registering, it may take up 60s because of this issue. for Cadence to load the new search attributes." 
+echo "If run the test too early, you may see error: \"IwfWorkflowType is not a valid search attribute key\""
+echo "and the test would fail with: unknown decision DecisionType: Activity, ID: 0, possible causes are nondeterministic workflow definition code or incompatible change in the workflow definition"
+sleep 65
+
+echo "now register the domain to tell the tests that Cadence is ready"
 for run in {1..60}; do
   echo "now trying to register domain in Cadence..."
   if cadence --do default domain register | grep -q 'Domain default already registered'; then
@@ -19,10 +25,5 @@ for run in {1..60}; do
   fi
   sleep 1
 done
-
-echo "After registering, it may take up 60s because of this issue. for Cadence to load the new search attributes." 
-echo "If run the test too early, you may see error: \"IwfWorkflowType is not a valid search attribute key\""
-echo "and the test would fail with: unknown decision DecisionType: Activity, ID: 0, possible causes are nondeterministic workflow definition code or incompatible change in the workflow definition"
-sleep 60
 
 tail -f /dev/null
