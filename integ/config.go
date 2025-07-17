93826ed5465862226a6846ab55a68b42deb73bac
Quanzheng Long <qlong@indeed.com>
2025-07-17 15:09:23 -0700

    IWF-1084: Fix TrimContextByTimeoutWithCappedDDL for long running api (#572)

--- integ/config.go
+++ integ/config.go
@@ -11,7 +11,7 @@ func createTestConfig(testCfg IwfServiceTestConfig) config.Config {
 	return config.Config{
 		Api: config.ApiConfig{
 			Port:           9715,
-			MaxWaitSeconds: 10, // use 10 so that we can test it in the waiting test
+			MaxWaitSeconds: 12, // use 12 so that we can test it in the waiting test
 			WaitForStateCompletionMigration: config.WaitForStateCompletionMigration{
 				SignalWithStartOn: "old",
 				WaitForOn:         "old",
